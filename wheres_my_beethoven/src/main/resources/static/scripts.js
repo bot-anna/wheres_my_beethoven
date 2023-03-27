f2a2e5ba2cc14ba27647740c998fffa6e6e59286
cyberknugen <anna.hakansson@pm.me>
2023-03-27 19:52:37 +0200

    added javascript for buttons and moved out about html to own file

--- wheres_my_beethoven/src/main/resources/static/scripts.js
+++ wheres_my_beethoven/src/main/resources/static/scripts.js
@@ -1,13 +1,31 @@
-    function loadContent(buttonId, containerId) {
-        var button = document.getElementById(buttonId);
-        var container = document.getElementById(containerId);
-        console.log("loadContent")
-    
-        button.addEventListener('click', function() {
-        fetch('./findconcert.html')
-            .then(response => response.text())
-            .then(html => {
-            container.innerHTML = html;
-            });
+function loadContent(buttonId, containerId, url) {
+    var button = document.getElementById(buttonId);
+    var container = document.getElementById(containerId);
+  
+      fetch(url)
+        .then(response => response.text())
+        .then(html => {
+          container.innerHTML = html;
+          addEventListeners(); // <-- call the function here
         });
-    }
+  }
+
+  function fetchAndDisplayContent() {
+    fetch(url)
+      .then(response => response.text())
+      .then(html => {
+        container.innerHTML = html;
+      });
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Add event listeners for all menu buttons
+    var container = document.getElementById('mainpane');
+    var url = 'about.html'
+      fetch(url)
+        .then(response => response.text())
+        .then(html => {
+          container.innerHTML = html;
+          addEventListeners(); // <-- call the function here
+        });
+  });
